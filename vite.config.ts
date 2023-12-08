2e5c16ec6a654184e9aea80d3f3bc45b28b4d16f
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-12-08 18:15:16 +0700

    chore: setup minimal config vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,32 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import type { PluginOption } from 'vite';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import type { VitePWAOptions } from 'vite-plugin-pwa';
+import { VitePWA } from 'vite-plugin-pwa';
 import tsConfigPaths from 'vite-tsconfig-paths';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  manifest: {
+    short_name: 'vite-react-tailwind-starter',
+    name: 'Vite React App Template',
+    lang: 'en',
+    start_url: '/',
+    background_color: '#FFFFFF',
+    theme_color: '#FFFFFF',
+    dir: 'ltr',
+    display: 'standalone',
+    orientation: 'portrait',
+    prefer_related_applications: false,
+    icons: [
+      {
+        src: '/assets/favicon.svg',
+        purpose: 'maskable any',
+        sizes: '48x48 72x72 96x96 128x128 256x256',
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -18,6 +43,7 @@ export default defineConfig({
     }),
     tsConfigPaths(),
     visualizer({ template: 'sunburst' }) as unknown as PluginOption,
+    VitePWA(pwaOptions),
   ],
   server: {
     open: true,
